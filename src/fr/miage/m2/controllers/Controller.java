3f37f4a6640d89ed3e59696bd7e64439e4b6e12a
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-11-05 16:26:13 +0100

    see view

--- src/fr/miage/m2/controllers/Controller.java
+++ src/fr/miage/m2/controllers/Controller.java
@@ -3,18 +3,37 @@ package fr.miage.m2.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.scene.control.Button;
 public class Controller implements Initializable {
     @FXML
+    private Button play;
+
+    @FXML
+    private Button rules;
+
+    @FXML
+    private Button about;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
-    openUserView
-            openRulesView
-    openAboutView
+    @FXML
+    public void openUserView(){
+
+    }
+
+    @FXML
+    public void openRulesView(){
+
+    }
+
+    @FXML
+    public void openAboutView(){
+
+    }
 }
