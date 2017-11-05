808c554eee5612a0675391db2dbcdc54e9483b59
Khaloudinho <khaled.bouguettoucha5@etu.univ-lorraine.fr>
2017-11-05 16:59:34 +0100

    translation into english

--- src/fr/miage/m2/Main.java
+++ src/fr/miage/m2/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
         //getClass().getResource("home.fxml")
         URL url = new File("src/fr/miage/m2/resources/home.fxml").toURL();
         Parent root = FXMLLoader.load(url);
-        primaryStage.setTitle("Dedal - BERTRAND - BOUGUETTOUCHA");
+        primaryStage.setTitle("Dedal Project");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
