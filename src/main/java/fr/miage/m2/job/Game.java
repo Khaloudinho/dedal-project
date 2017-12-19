4e5ea522e31803c66e966b69bf06559a1e2df156
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2017-12-19 21:09:45 +0100

    feauture:project:convert to a maven project

---
+++ src/main/java/fr/miage/m2/job/Game.java
@@ -0,0 +1,72 @@
+package fr.miage.m2.job;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class which represent a Game
+ * It is in charge of manage turn system and link with the player
+ */
+public class Game implements Serializable {
+
+	private Player player = new Player();
+
+	Points point;
+
+	List<Dice> dices = new ArrayList<Dice>();
+
+	private int indexCurrentPlayer=0;
+
+	private int NUMBER_OF_TURN=10;
+	private int currentTurn=0;
+
+	private static class GameWrapper{
+		private static Game instanceGame = new Game();
+	}
+
+	public static Game getInstance(){
+		return GameWrapper.instanceGame;
+	}
+
+	public Game() {
+	}
+
+	public Points getPoint() {
+		return point;
+	}
+
+	public void setPoint(Points point) {
+		this.point = point;
+	}
+
+	public List<Dice> getDices() {
+		return dices;
+	}
+
+	public void setDices(List<Dice> dices) {
+		this.dices = dices;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public int getNUMBER_OF_TURN() {
+		return NUMBER_OF_TURN;
+	}
+
+	public int getCurrentTurn() {
+		return currentTurn;
+	}
+
+	public void doTurn(){
+		Player player = this.getCurrentPlayer();
+		System.out.println(player.getFirstname()+"_"+player.getLastname()+" joue..");
+		this.currentTurn++;
+	}
+
+	public Player getCurrentPlayer(){
+		return this.player;
+	}
+}
